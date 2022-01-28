81eb5b64bc51b680b2658762d8afeb11bd4371d0
Matt Simpson <matt.rc.simpson@gmail.com>
2022-01-28 10:23:25 +0200

    create addTask function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log("connected");
+
+// item | description | date added | completed | delete
+
+var deleteTodo = function () {
+    console.log("Delete task test");
+}
+
+var addTask = function () {
+    var table = document.getElementById("table");
+    var item = document.getElementById("item").value;
+    var description = document.getElementById("description").value;
+    var date = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
+
+
+    var todo = document.createElement('tr');
+    todo.innerHTML = `
+        <td>${item}</td>
+        <td>${description}</td>
+        <td>${date}</td>
+        <td>Test data</td>
+        <td><span id="deleteTaskSpan" onclick="deleteTodo()">X</span></td>
+    `;
+    table.appendChild(todo);
+
+    item.value = " ";
+    description.value = " ";
+}
