e2469fa8139a7e44bd3d64cb852a9ea7950db0ca
Matt Simpson <matt.rc.simpson@gmail.com>
2022-01-31 19:05:58 +0200

    localStorage test

--- js/script.js
+++ js/script.js
@@ -7,14 +7,23 @@ var addTask = function () {
     var item = document.getElementById("item").value;
     var description = document.getElementById("description").value;
     var date = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
+    
+    localStorage.setItem('item', `${item}`);
+    localStorage.setItem('description', `${description}`);
+    localStorage.setItem('date', `${date}`);
+
+    // <h2>${item}</h2>
+    //     <p>${description}</p>
+    //     <p>${date}</p>
+
 
     var todo = document.createElement('div');
     todo.className += 'note';
 
     todo.innerHTML = `
-        <h2>${item}</h2>
-        <p>${description}</p>
-        <p>${date}</p>
+        <h2>${localStorage.getItem("item")}</h2>
+        <p>${localStorage.getItem("description")}</p>
+        <p>${localStorage.getItem("date")}</p>
         <p>Complete</p>
         <p class="testOne" id="deleteTaskCursor" onclick="deleteTodo()">Delete task</p>
     `;
