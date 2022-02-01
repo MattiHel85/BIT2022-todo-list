757d5e5ad2b40b860f25d011ade98f7388a92925
guspi <guspielmichal@gmail.com>
2022-02-01 13:44:43 +0200

    Added status attribute to Note class

--- js/note.js
+++ js/note.js
@@ -2,11 +2,12 @@
  * As name suggests this is the data class which represents each note on the website,
 */
 class Note{
-constructor(title,description,date,color){
+constructor(title,description,date,color,status){
 this.title = title;
 this.description = description;
 this.date = date;
 this.color = color;
+this.status = status;
 }
 
 }
