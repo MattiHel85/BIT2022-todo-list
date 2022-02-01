62a058f58e6278d9ff221981651b6c2901e7b992
guspi <guspielmichal@gmail.com>
2022-02-01 13:35:36 +0200

    Implemented logic for checkin input validity

--- js/script.js
+++ js/script.js
@@ -43,6 +43,24 @@ var addNewNote = function () {
     var description = document.getElementById("description").value;
     var date = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
     
+
+    /**THIS CHECKS IF INPUT IS VALID AND REACTS FOR IT ACCORDINGLY
+     * I WOULD LIKE TO HAVE THIS LOGIC AS SEPERATE FUNCTION BUT I DON'T KNOW HOW TO RETURN THIS FUNCTION EARLY WITH ANOTHER FUNCTION
+    */
+    if( item.length < 2 && description.length < 2){
+        errorAnimation("item");
+        errorAnimation("description");
+        return;
+    }
+    if(item.length < 2){
+        errorAnimation("item");
+        return;
+    }
+    if(description.length < 2){
+        errorAnimation("description");
+        return;
+    }
+
     //LOAD LIST OF NOTES FROM THE LOCAL SOTRAGE
     var oldListLocalStorage = localStorage.getObj(noteList)
     //CREATE NEW NOTE
@@ -80,4 +98,14 @@ var deleteTodo = function (id) {
 var clearTasks = function(){
     localStorage.clear()
     refreshNotesCards()
+}
+
+
+var errorAnimation = function(elementId){
+    document.getElementById(elementId).classList.add("bounce");
+    setTimeout(function() {
+      //remove the class so animation can occur as many times as user triggers event, delay must be longer than the animation duration and any delay.
+      document.getElementById(elementId).classList.remove("bounce");
+    }, 1000);   
+
 }
